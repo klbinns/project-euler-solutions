acb2c5777abd47426a06ed54122fe44ac79a0763
klbinns <3086306+klbinns@users.noreply.github.com>
2013-10-30 10:08:14 -0400

    Problem20

---
+++ Solutions/Problem20.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import math
+
+'''
+Problem 20
+
+Find the sum of the digits in the number 100!
+
+'''
+
+fact_100 = math.factorial(100)
+
+def sum_digits(n):
+    s = 0
+    while n:
+        s += n % 10
+        n /= 10
+    return s
+
+print sum_digits(fact_100) # 648
