4fbcdfe0652fb3c3949eb33feee24d88c6eb67c5
klbinns <3086306+klbinns@users.noreply.github.com>
2013-10-30 10:48:18 -0400

    Solution to Problem25.py

---
+++ Solutions/Problem25.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+'''
+Problem 25
+
+What is the first term in the Fibonacci sequence to contain 1000 digits?
+
+'''
+
+
+a = 1
+b = 1
+c = 0
+index = 2
+
+while len(str(c)) < 1000:
+
+    c = a + b
+    index += 1 
+    a = b
+    b = c
+ 
+print index #4782
