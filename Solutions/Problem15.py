4b28fd5b95b0ba9cd5909063f9eef023f2efdb71
klbinns <3086306+klbinns@users.noreply.github.com>
2019-04-13 23:33:15 +0800

    Improved solutions Problem 15

--- Solutions/Problem15.py
+++ Solutions/Problem15.py
@@ -12,25 +12,13 @@ How many such routes are there through a 20×20 grid?
 cache = {}
 
 def num_of_routes(x, y):
-  if x == 0 and y == 0:
+  if x == 0 or y == 0:
     return 1
   
-  left = 0
-  if x > 0:
-    if (x-1, y) in cache:
-      left = cache[(x-1, y)]
-    else:
-      left = num_of_routes(x-1, y)
+  if (x, y) in cache:
+    return cache[(x, y)]
 
-  right = 0
-  if y > 0:
-    if (x, y-1) in cache:
-      right = cache[(x, y-1)]
-    else:
-      right = num_of_routes(x, y-1)
+  cache[(x, y)] = num_of_routes(x-1, y) + num_of_routes(x, y-1)
+  return cache[(x, y)]  
   
-  count = left + right
-  cache[(x, y)] = count
-  return count
-
 print(num_of_routes(20, 20)) # 137846528820
