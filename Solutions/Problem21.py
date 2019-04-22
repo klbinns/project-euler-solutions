2802b304f6312bed410e0a019f95f0faf09553af
klbinns <3086306+klbinns@users.noreply.github.com>
2019-04-22 09:09:06 +0800

    Problem 21 improvements

--- Solutions/Problem21.py
+++ Solutions/Problem21.py
@@ -11,7 +11,6 @@ The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 Evaluate the sum of all the amicable numbers under 10000.
 '''
 d_evals = {}
-amicable_numbers = set()
 
 def divisors(n):
   i = 1
@@ -29,10 +28,10 @@ def d(n):
     d_evals[n] = sum(divisors(n))
     return d_evals[n]
 
+amicable_numbers_sum = 0
 for a in range(1, 10001):
   b = d(a)
-  if a != b and a == d(b):
-    amicable_numbers.add(a)
-    amicable_numbers.add(b)
+  if b > a and a == d(b):
+    amicable_numbers_sum += (a + b)
 
-print(sum(list(amicable_numbers))) # 31626
+print(amicable_numbers_sum) # 31626
