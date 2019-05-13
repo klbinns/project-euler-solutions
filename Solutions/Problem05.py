d2d4924b0944fa40866b9904da46b1f96071b985
klbinns <3086306+klbinns@users.noreply.github.com>
2019-05-13 08:56:11 +0800

    Problem 5 refactoring

--- Solutions/Problem05.py
+++ Solutions/Problem05.py
@@ -7,21 +7,17 @@ Problem 5:
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 '''
 
-# may be able to convert this to a recursive function
+def isDivisible(n, divisor):
+  return n % divisor == 0
+
+def isRangeDivisble(n, y, x):
+  return all(isDivisible(n, i) for i in range(y, x, -1))
 
 i = 20
 
 while True:
-    if i % 19 == 0:
-        if i % 18 == 0:
-            if i % 17 == 0:
-                if i % 16 == 0:
-                    if i % 15 == 0:
-                        if i % 14 == 0:
-                            if i % 13 == 0:
-                                if i % 12 == 0:
-                                    if i % 11 == 0:
-                                        print(i) #232792560
-                                        break
+  if isRangeDivisble(i, 19, 10):
+    print(i) # 232792560
+    break
+  else:
     i += 20
-
